5a9e76574f51230650d47f48536970cc7af2994f
dull69 <dull69@mail.ru>
2022-11-10 20:21:58 +0300

    SPRINT_1: add server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, './dist')));
+
+app.get('/', (_, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
